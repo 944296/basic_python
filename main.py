9ab5c76ea5513299556f014ea733d7fac3767af2
944296 <124764945+944296@users.noreply.github.com>
2023-03-05 23:47:40 +0900

    main

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-from requests import get
+# 봇으로 인식해서 request할 수 없을 경우 => 셀레니움 설치(브라우저 자동화)!!
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+# python에서 html 읽을 수있도록 => Beautifulsoup 설치!!
 from bs4 import BeautifulSoup
+# 다른 폴더에있는 파일을 main파일로 가져옴
 from extractors.wwr import extract_wwr_job
 
-base_url = "https://kr.indeed.com/jobs?q="
+Options = Options()
+browser = webdriver.Chrome(options=Options)
+
+base_url = "https://kr.indeed.com/job?q="
 search_term = "python"
+browser.get(f"{base_url}{search_term}")
+results = []
+soup = BeautifulSoup(browser.page_source, "html.parser")
+job_list = soup.find("ul", class_="jobsearch-ResultsList")
+# recursive=False => 한단계 깊이까지의 li만!!
+jobs = job_list.find_all("li", recursive=False)
+for job in jobs:
+    zone = job.find("div", class_="mosaic-zone")
+    if zone == None:
+        anchor = job.select_one("a")
+        title = anchor['aria-label']
+        link = anchor["href"]
+        company = job.find("span", class_="comapanyName")
+        location = job.find("div", class_="companyLocation")
+        job_data = {
+            "link": f"https://kr.indeed.com{link}",
+                    "company": company.string,
+                    "location": location.string,
+                    "position": title
+        }
+        results.append(job_data)
 
-response = get(f"{base_url}{search_term}")
-
-if response.status_code != 200:
-    print("Can't request page")
-else:
-    soup = BeautifulSoup(response.text, "html.parser")
-    job_list = soup.find("ul", class_="jobsearch-ResultsList")
-    jobs = job_list.find_all("li", recursive=False)  # ul의 바로 아래 li만!!!
-    print(len(jobs))
-    for job in jobs:
-        print(job)
+for result in results:
+    print(result, "/////\n")
